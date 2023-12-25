418d9b2d96be83113be4e80f7931adf970ec8dad
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-12-25 21:46:13 +0600

    count pages and add dynamic buttons

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,11 +3,21 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
+    const {count} = useLoaderData();
+    // console.log(count)
+
+    const itemsPerPage = 10;
+    const numberOfPages = Math.ceil(count / itemsPerPage)
+
+    const pages = [...Array(numberOfPages).keys()];
+    console.log(pages)
+
+
 
     useEffect(() => {
         fetch('http://localhost:5000/products')
@@ -81,6 +91,12 @@ const Shop = () => {
                         <button className='btn-proceed'>Review Order</button>
                     </Link>
                 </Cart>
+            </div>
+            <div className='pagination'>Pagination
+            {
+                pages.map(page=><button key={page}>{page}</button>)
+            }
+
             </div>
         </div>
     );
